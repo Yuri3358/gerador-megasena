51cb5cfdcbea2aa8bab2377c5be945fe7e70f3ef
Yuri3358 <yurialmeida917@gmail.com>
2022-12-11 18:22:27 -0300

    feat: random numbers array generator

--- script.js
+++ script.js
@@ -1,20 +1,25 @@
 const root = ReactDOM.createRoot(document.getElementById("root"))
 
 function App() {
+    function GenNumbers() {
+        const list = []
+        for (let i = 0; i < 6; i++) {
+            list.push(Math.round(Math.random()*60))
+        }
+        console.log(list)
+    }
+
     return (
         <section id="content">
             <h1>Gerador números da Mega-Sena</h1>
             <div id="result"></div>
-            <button>Generate</button>
+            <button onClick={() => GenNumbers()}>Generate</button>
         </section>
     )
 }
 
 /*
-const list = []
-for (let i = 0; i < 6; i++) {
-    list.push(Math.round(Math.random()*60))
-}
+
 */
 
 root.render(<App/>)
