5e71e177f0014c877a906576903b42241e95b376
Yuri3358 <yurialmeida917@gmail.com>
2022-12-11 18:43:18 -0300

    feat: dynamic list numbers color span

--- script.js
+++ script.js
@@ -2,19 +2,21 @@ const root = ReactDOM.createRoot(document.getElementById("root"))
 
 function App() {
     const [listNumbers, setListNumbers] = React.useState("Clique em gerar")
+    const [listColor, setListColor] = React.useState()
     function GenNumbers() {
         const list = []
         for (let i = 0; i < 6; i++) {
             list.push(Math.round(Math.random()*60))
         }
         setListNumbers(list.join(" "))
+        setListColor({color: "yellow"})
     }
 
     return (
         <section id="content">
             <h1>Gerador de números da Mega-Sena</h1>
             <div id="result">
-                <span id="generated_numbers">{listNumbers}</span>
+                <span style={listColor}>{listNumbers}</span>
             </div>
             <button id="generate" onClick={() => GenNumbers()}>Sortar números</button>
         </section>
