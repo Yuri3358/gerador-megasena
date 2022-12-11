c3c22c3a525716a5e17009e325a73b251eb29d1d
Yuri3358 <yurialmeida917@gmail.com>
2022-12-11 18:30:12 -0300

    feat: listNumbers state solving scope problems

--- script.js
+++ script.js
@@ -1,25 +1,24 @@
 const root = ReactDOM.createRoot(document.getElementById("root"))
 
 function App() {
+    const [listNumbers, setListNumbers] = React.useState("Clique em gerar")
     function GenNumbers() {
         const list = []
         for (let i = 0; i < 6; i++) {
             list.push(Math.round(Math.random()*60))
         }
-        console.log(list)
+        setListNumbers(list.join(" "))
     }
 
     return (
         <section id="content">
-            <h1>Gerador números da Mega-Sena</h1>
-            <div id="result"></div>
-            <button onClick={() => GenNumbers()}>Generate</button>
+            <h1>Gerador de números da Mega-Sena</h1>
+            <div id="result">
+                <span id="generated_numbers">{listNumbers}</span>
+            </div>
+            <button onClick={() => GenNumbers()}>Gerar</button>
         </section>
     )
 }
-
-/*
-
-*/
 
 root.render(<App/>)
